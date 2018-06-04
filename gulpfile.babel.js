9aa3c18e274138c76b6d102dc49a8bd8a00bb1d7
Maksym Tymchyk <maksym.tymchyk@gmail.com>
2018-06-04 17:43:41 +0300

    Initial commit of v2

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,24 +1,31 @@
 import gulp from 'gulp';
-import Path from 'path';
+import path from 'path';
+
+import * as PotExtractor from './pot-extractor';
 
 const PATH = {
-    SOURCE: Path.join(__dirname, './src'),
-    TARGET: Path.join(__dirname, './dist'),
-    PUBLIC: Path.join(__dirname, './public')
+    SOURCE: path.join(__dirname, './src'),
+    TARGET: path.join(__dirname, './dist'),
+    PUBLIC: path.join(__dirname, './public')
 };
 
 gulp.task('copy', copyTask({
     source: './public/',
-    destinations: [
-        './dist/public'
-    ],
+    destinations: ['./dist/public'],
     // pattern: '/*',
 }));
 
+gulp.task('locales', copyTask({
+    source: './locales/',
+    destinations: ['./dist/locales'],
+    // pattern: '/*',
+}));
 
-gulp.task('watch', ['copy'], function () {
-    gulp.watch('./public', ['copy']);
+gulp.task('pot', () => {
+    PotExtractor.extract();
 });
+
+gulp.task('build', ['copy', 'locales']);
 
 function copyTask(opts) {
     const {
@@ -29,7 +36,7 @@ function copyTask(opts) {
     } = opts;
 
     return () => {
-        let stream = gulp.src(source + pattern, { base: source });
+        let stream = gulp.src(source + pattern, {base: source});
         destinations.forEach((destination) => {
             stream = stream.pipe(gulp.dest(destination))
         });
--- src/server/controllers/index.ts
+++
@@ -1,7 +0,0 @@
-import * as Home from './home-controller';
-import * as UIKit from './ui-kit-controller';
-
-export {
-    Home,
-    UIKit,
-};
